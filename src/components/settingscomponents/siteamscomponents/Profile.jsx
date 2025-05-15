0acfd7d7711f5eb4950045e1e82e0e5fbfb7224f
Chamith12345 <chamithanalanka29@gmail.com>
2025-05-15 16:25:55 +0530

    proifle page done

--- src/components/settingscomponents/siteamscomponents/Profile.jsx
+++ src/components/settingscomponents/siteamscomponents/Profile.jsx
@@ -4,14 +4,33 @@ import './profile.css';
 export default function Profile() {
   return (
     <div className='main'>
-      <div className="profilebackgroung">
-      <img className='profilephoto' src={profilephoto}  alt="error"  />
-      </div>
-      <div className="profledetails">   <p className='profilep1'>Name</p> <p className='profilep2'>Chamith Analanka</p> </div>
-      <div className="profledetails">   <p className='profilep1'>Adderss</p> <p className='profilep2'>Chamith Analanka</p> </div>
-      <div className="profledetails">   <p className='profilep1'>Number</p> <p className='profilep2'>Chamith Analanka</p> </div>
-      <div className="profledetails">   <p className='profilep1'>Gender</p> <p className='profilep2'>Chamith Analanka</p> </div>
-      </div>
+        <div className="profilebackgroung">
+        <img className='profilephoto' src={profilephoto}  alt="error"  />
+        </div>
+          <div className="profilealldetails">
+             <div className="profledetails">   
+                <p className='profilep1'>Name</p> 
+                <p className='profilep2'>Chamitlanka</p> 
+            </div>
+             <div className="profledetails">   
+                <p className='profilep1'>Adderss</p> 
+                <p className='profilep2'>440, walpala road andiambalama</p> 
+            </div>
+             <div className="profledetails">   
+                <p className='profilep1'>Number</p> 
+                <p className='profilep2'>0772809408</p> 
+            </div>
+             <div className="profledetails">   
+                <p className='profilep1'>Company Name</p> 
+                <p className='profilep2'>draaa</p> 
+              </div>
+
+            <div className="settingbutton">
+              <button className='b1'> Edit</button>
+            </div>
+          </div>
+    </div>
     
   )
 }
+
